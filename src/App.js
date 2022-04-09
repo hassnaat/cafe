6092093f4fc4f81c342a66dd3611e46de018c48e
hassnaat <hassnaatfarooq@gmail.com>
2022-04-09 13:10:58 +0500

    protected routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import { useState, useEffect } from "react"
 import Home from './screens/Home/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './screens/Auth/Login';
 import Signup from './screens/Auth/Signup';
 import AdminLogin from './screens/Auth/AdminLogin';
@@ -9,8 +10,20 @@ import Checkout from './screens/Checkout/Checkout';
 import AdminSignup from './screens/Auth/AdminSignup';
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const user = useSelector(state => state.user)
+  const [authenticated, setAuthenticated] = useState();
+  const [isAdmin, setIsAdmin] = useState();
+  useEffect(() => {
+    if (localStorage.getItem("user") !== null) {
+      setAuthenticated(true)
+    }
+    else {
+      setAuthenticated(false)
+    }
+  }, [])
   return (
     <Router>
       <div className="App">
@@ -26,13 +39,13 @@ function App() {
           pauseOnHover
         />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/admin/signup" element={<AdminSignup />} />
-          <Route path="/children" element={<Children />} />
-          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/admin/login" element={!authenticated ? <AdminLogin /> : <Navigate replace to="/" />} />
+          <Route path="/login" element={!authenticated ? <Login /> : <Navigate replace to="/children" />} />
+          <Route path="/signup" element={!authenticated ? <Signup /> : <Navigate replace to="/children" />} />
+          <Route path="/admin/signup" element={!authenticated ? <AdminSignup /> : <Navigate replace to="/" />} />
+          <Route path="/" element={authenticated ? <Home /> : <Navigate replace to="/admin/login" />} />
+          <Route path="/children" element={authenticated ? <Children /> : <Navigate replace to="/login" />} />
+          <Route path="/checkout" element={authenticated ? <Checkout /> : <Navigate replace to="/login" />} />
         </Routes>
       </div>
     </Router>
